70e6373b34bbde6da9934338bb633ec2de10ab11
stan15 <stanihe1901@gmail.com>
2022-01-16 21:08:23 -0500

    attached linter to editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,30 +1,64 @@
 import { basicSetup, EditorView } from '@codemirror/basic-setup';
-import React, { useEffect, useRef } from 'react';
-import { EditorState, Compartment } from '@codemirror/state';
-import { tablature } from 'lang-tablature';
+import React, { useState, useEffect, useRef } from 'react';
+import { EditorState, Compartment, StateEffect } from '@codemirror/state';
+import { linter } from '@codemirror/lint';
+import { tablature, tabLint } from 'lang-tablature';
 import './editor.css';
 
-function Editor(props:any) {
+type Props = {
+    delay?: number
+}
+
+let defaultProps = {
+    delay: 500
+}
+
+let tablintrc = {};
+function createLinterExtension(tablintrc:any) {
+    return linter(tabLint(tablintrc), {
+        delay: 500
+    });
+}
+
+let lintCompartment = new Compartment();
+let langCompartment = new Compartment();
+
+function Editor(props:Props = defaultProps) {
     const editorRef = useRef<HTMLDivElement>(null);
+    const [view, setView] = useState<EditorView>();
 
     useEffect(() => {
-        let language = new Compartment();
-        const state = EditorState.create({
+        const s = EditorState.create({
             doc: "hello! ",
             extensions: [
                 basicSetup,
-                language.of(tablature())
+                langCompartment.of(tablature()),
             ]
         });
-        const view = new EditorView({
-            state,
+        const v = new EditorView({
+            state: s,
             parent: editorRef.current!
-        })
-
+        });
+        setView(v);
         return () => {
-            view.destroy();
+            v?.destroy();
         }
-    }, [])
+    }, []);
+
+    //updating linter configuration
+    useEffect(() => {
+        let lintExtension = createLinterExtension(tablintrc);
+        if (view && lintCompartment.get(view.state)) {
+            view?.dispatch({
+                effects: lintCompartment.reconfigure(lintExtension)
+            });
+        }else {
+            view?.dispatch({
+                effects: StateEffect.appendConfig.of(lintExtension)
+            })
+        }
+    }, [view, tablintrc]);
+
     return (
         <div className='editor' ref={editorRef} />
     )
